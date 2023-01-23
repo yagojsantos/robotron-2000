abe6a464cfd1ae4a61c8bf77bc5c73a83f207b99
Yago de Jesus Santos <yago_jesus@outlook.com.br>
2023-01-23 09:15:56 -0300

    Controles
    Foi iniciada a codificação necessária para identificar os botões de controle de elementos do robotrom.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
-const robotron = document.querySelector("#robotron")
+const subtrair = document.querySelector("#subtrair")
+const somar = document.querySelector("#somar")
+const braco = document.querySelector("#braco")
 
-robotron.addEventListener,("click", dizOi)
+const controle = document.querySelectorAll(".controle-ajuste")
 
-function dizOi() {
-    console.log('oi')
+somar.addEventListener("click",()=>{manipulaDados("somar")})
+subtrair.addEventListener("click",()=>{manipulaDados("subtrair")})
+
+
+function manipulaDados(operacao){
+    if(operacao === "subtrair"){
+        braco.value = parseInt(braco.value) - 1
+    } else {
+        braco.value = parseInt(braco.value) + 1
+    }
 }
